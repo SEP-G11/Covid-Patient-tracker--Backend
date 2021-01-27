d5c853c3b17578853b4bb83bf31515c82298558c
mahela97 <m.pradeepd@gmail.com>
2021-01-27 18:51:28 +0530

    mahela-1

--- controller/userController.js
+++ controller/userController.js
@@ -1,89 +1,86 @@
-const {genSaltSync,hashSync,compareSync} = require ("bcrypt");
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
-const {createRegisteredUser,getRegistedUserByEmail,getRegistedUserById} =require ("../service/userService");
+const {
+  createRegisteredUser,
+  getRegistedUserByEmail,
+  getRegistedUserById,
+} = require("../service/userService");
 
-module.exports={
-    createUser:(req,res)=>{
-        const body=req.body;
-        const salt = genSaltSync(10);
-        body.password=hashSync(body.password,salt)
-        if(body.name && body.email && body.birthday && body.contact_no && body.passport_no && body.country && body.password && body.user_photo){
-            createRegisteredUser(body,(err,result)=>{
-                if(err){
-                    console.log(err)
-                    if(err.code=="ER_DUP_ENTRY"){
-                        return res.json({
-                            sucess:0,
-                            message:"email already exist"
-                        })
-                    }
-                    return res.json({
-                        sucess:0,
-                        message:err
-                    })
-                }
-                else{
-                    return res.json({
-                        sucess:1,
-                        data:result,
-                        message:"SignUp successful"
-                    })
-                }
-            })
-        }else{
-            return res.json({
-                sucess:0,
-                message:"Missing Fields"
-            })
-        }
-        
-    },
+module.exports = {
+  createUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
 
-    loginUser: async (req,res)=>{
-        const body=req.body
-        let userDetailsinDatabase;
-        try{
-            userDetailsinDatabase=await getRegistedUserByEmail(body.email)
-            if(userDetailsinDatabase){
-                const result=compareSync(body.password,userDetailsinDatabase.password)
-                if(result){
-                    userDetailsinDatabase.password=undefined;
-                    const jsontoken=sign({result:userDetailsinDatabase},"qwe1234",{
-                        expiresIn:"1h"
-                    })
-                    return res.json({
-                        sucess:1,
-                        message:"login Sucess",
-                        token:jsontoken
-                    })
-                }else{
-                    return res.json({
-                        sucess:0,
-                        message:"Password is invalid"
-                    })
-                }
-            }else{
-                return res.json({
-                    sucess:0,
-                    message:"Invalid Email"
-                })
-            }
+    createRegisteredUser(body, (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code == "ER_DUP_ENTRY") {
+          return res.json({
+            sucess: 0,
+            message: "email already exist",
+          });
         }
-        catch(err){
-            console.log(err)
-            return res.json({
-                sucess:0,
-                message:err
-            })
-        }
-    },
-    getUserProfile:async (req,res)=>{
-        userDetails = await getRegistedUserById(req.user.user_id)
-        console.log(userDetails)
         return res.json({
-            success:1,
-            data:{...userDetails,password:undefined}
-        })
+          sucess: 0,
+          message: err,
+        });
+      } else {
+        return res.json({
+          sucess: 1,
+          data: result,
+        });
+      }
+    });
+  },
 
+  loginUser: async (req, res) => {
+    const body = req.body;
+    let userDetailsinDatabase;
+    try {
+      userDetailsinDatabase = await getRegistedUserByEmail(body.email);
+      if (userDetailsinDatabase) {
+        const result = compareSync(
+          body.password,
+          userDetailsinDatabase.password
+        );
+        if (result) {
+          userDetailsinDatabase.password = undefined;
+          const jsontoken = sign({ result: userDetailsinDatabase }, "qwe1234", {
+            expiresIn: "1day",
+          });
+          return res.json({
+            sucess: 1,
+            message: "login Sucess",
+            token: jsontoken,
+          });
+        } else {
+          return res.json({
+            sucess: 0,
+            message: "Password is invalid",
+          });
+        }
+      } else {
+        return res.json({
+          sucess: 0,
+          message: "Invalid Email",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.json({
+        sucess: 0,
+        message: err,
+      });
     }
-}
+  },
+  getUserProfile: async (req, res) => {
+    userDetails = await getRegistedUserById(req.user.user_id);
+    console.log(userDetails);
+    return res.json({
+      success: 1,
+      data: { ...userDetails, password: undefined },
+    });
+  },
+  bookSeat: async (req, res) => {},
+};
--- service/flightService.js
+++ service/flightService.js
@@ -1,32 +1,55 @@
-const pool=require("../config/database")
+const pool = require("../config/database");
 
-module.exports={
-    getFlightSchedule(callBack){
-        pool.query(
-            `SELECT * FROM (SELECT * FROM flight_schedule natural join route) as a join (SELECT airport_id,country as origin_country,state  as origin_state ,city as origin_city  FROM airport) as b ON a.origin=b.airport_id join (SELECT airport_id,country as destination_country,state  as destination_state ,city as destination_city  FROM airport) as c ON a.destination=c.airport_id order by date desc`,
-            [],
-            (err,result)=>{
-                if(err){
-                    return callBack(err)
+module.exports = {
+  getFlightSchedule(callBack) {
+    pool.query(
+      `SELECT * FROM (SELECT * FROM flight_schedule natural join route) as a join (SELECT airport_id,country as origin_country,state  as origin_state ,city as origin_city  FROM airport) as b ON a.origin=b.airport_id join (SELECT airport_id,country as destination_country,state  as destination_state ,city as destination_city  FROM airport) as c ON a.destination=c.airport_id order by date desc`,
+      [],
+      (err, result) => {
+        if (err) {
+          return callBack(err);
+        } else {
+          return callBack(null, result);
+        }
+      }
+    );
+  },
+  getFlightInfo(flight_id, callback) {
+    pool.query(
+      `SELECT * FROM (SELECT * FROM flight_schedule natural join route order by date desc) as a join (SELECT airport_id,country as origin_country,state  as origin_state ,city as origin_city  FROM airport) as b ON a.origin=b.airport_id join (SELECT airport_id,country as destination_country,state  as destination_state ,city as destination_city  FROM airport) as c ON a.destination=c.airport_id where flight_id=?`,
+      [flight_id],
+      (err, result) => {
+        if (err) {
+          return callback(err);
+        } else {
+          pool.query(
+            `SELECT SEAT_ID,CLASS_NAME FROM SEAT_INFO NATURAL JOIN TRAVEL_CLASS WHERE AIRCRAFT_ID=? ORDER BY SEAT_ID;SELECT SEAT_ID FROM BOOKING WHERE FLIGHT_ID=? `,
+            [result[0].aircraft_id, flight_id],
+            (err, results) => {
+              if (err) {
+                return callback(err);
+              } else {
+                result[0].seat_info = {};
+                const seats = { ...results[0] };
+                const bookedSeats = [];
+                for (var j = 0; j < results[1].length; j++) {
+                  bookedSeats.push(results[1][j].SEAT_ID);
                 }
-                else{
-                    return callBack(null,result)
+                for (var i = 0; i < results[0].length; i++) {
+                  result[0].seat_info[seats[i].SEAT_ID] = seats[i];
+                  if (bookedSeats.includes(seats[i].SEAT_ID)) {
+                    result[0].seat_info[seats[i].SEAT_ID].isAvailable = false;
+                  } else {
+                    result[0].seat_info[seats[i].SEAT_ID].isAvailable = true;
+                  }
                 }
+
+                return callback(null, result);
+              }
             }
-        )
-    },
-    getFlightInfo(flight_id,callback){
-        pool.query(
-            `SELECT * FROM (SELECT * FROM flight_schedule natural join route order by date desc) as a join (SELECT airport_id,country as origin_country,state  as origin_state ,city as origin_city  FROM airport) as b ON a.origin=b.airport_id join (SELECT airport_id,country as destination_country,state  as destination_state ,city as destination_city  FROM airport) as c ON a.destination=c.airport_id where flight_id=?`,
-            [flight_id],
-            (err,result)=>{
-                if(err){
-                    return callback(err)
-                }
-                else{
-                    return callback(null,result[0])
-                }
-            }
-        )
-    }
-}
+          );
+        }
+      }
+    );
+  },
+};
