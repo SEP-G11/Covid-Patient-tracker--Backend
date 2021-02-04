c54069417007c9a759e5a77f6db36cd6eaaadead
mahela97 <m.pradeepd@gmail.com>
2021-02-04 17:39:51 +0530

    final-3

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -1,5 +1,4 @@
 const { verify } = require("jsonwebtoken");
-const { getFlightInfo } = require("../service/flightService");
 
 module.exports = {
   checkToken: (req, res, next) => {
@@ -26,7 +25,7 @@ module.exports = {
   },
   setUserIfLoggedIn: async (req, res, next) => {
     let token = req.get("authorization");
-    const result = await getFlightInfo(req.params.flight_id);
+
     if (token) {
       token = token.slice(7);
       verify(token, "qwe1234", (err, decoded) => {
@@ -37,10 +36,6 @@ module.exports = {
           });
         } else {
           req.user = decoded.result;
-          const seat_data = result[0].seat_info;
-          if (req.params.seat_id) {
-            req.seat_data = seat_data[req.params.seat_id].isAvailable;
-          }
           next();
         }
       });
--- middlewares/isBooked.js
+++ middlewares/isBooked.js
@@ -1,5 +1,12 @@
+const { getFlightInfo } = require("../service/flightService");
+
 module.exports = {
-  isBooked: (req, res, next) => {
+  isBooked: async (req, res, next) => {
+    const result = await getFlightInfo(req.params.flight_id);
+    const seat_data = result[0].seat_info;
+    if (req.params.seat_id) {
+      req.seat_data = seat_data[req.params.seat_id].isAvailable;
+    }
     if (req.seat_data) {
       next();
     } else {
