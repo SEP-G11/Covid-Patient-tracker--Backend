ea9142d421a0f9d7cd7c95bccc31163d8ea321c5
malithi-lalanika <66944661+malithi-lalanika@users.noreply.github.com>
2021-02-17 09:15:48 +0530

    Update Query.sql

--- database/Query.sql
+++ database/Query.sql
@@ -86,4 +86,60 @@ BEGIN
 END$$
 
 DELIMITER ;
+											  
+--get number of guest passengers
 
+DELIMITER $$
+ CREATE FUNCTION get_Number_Of_Guest_Passengers  (start_date date ,end_date date) 
+ RETURNS INT
+ DETERMINISTIC
+    BEGIN 
+
+ 	declare p_count integer;    
+	select count(*) into p_count
+	from flight_schedule natural join booking natural join user where date BETWEEN start_date AND end_date AND is_registered=0 ;
+    
+	return p_count;
+
+    END$$
+DELIMITER ;
+
+--get number of registerd passengers group by their packages
+DELIMITER $$
+  CREATE PROCEDURE get_Number_Of_Registered_Passengers(start_date date ,end_date date)
+  
+    BEGIN
+    
+        SELECT package_name,count(*) as num_of_passengers FROM flight_schedule natural join booking natural join profile where date BETWEEN start_date AND end_date group by package_name;
+        
+    END$$
+DELIMITER ;
+
+--get all past flights details
+
+DELIMITER $$
+  CREATE PROCEDURE get_All_Pass_Flights(in_origin varchar(20), in_destination varchar(20))
+  
+    BEGIN
+    
+        SELECT * FROM flight_schedule natural join route where date<CURDATE() and origin=in_origin and destination=in_destination;
+        
+    END$$
+DELIMITER ;
+
+--get passenger count of the past flights
+
+DELIMITER $$
+ CREATE FUNCTION get_Passenger_Count_O_D  (in_origin varchar(20), in_destination varchar(20)) 
+ RETURNS INT
+ DETERMINISTIC
+    BEGIN 
+
+ 	declare p_count integer;    
+	select count(*) into p_count
+	from booking natural join flight_schedule natural join route where date<CURDATE() and origin=in_origin and destination=in_destination ;
+    
+	return p_count;
+
+    END$$
+DELIMITER ;
