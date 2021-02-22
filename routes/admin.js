d454ec67023ed91597067d925945f1b1090a351d
LahiruKumaraHewagama <lahiruhewagama98@gmail.com>
2021-02-22 05:42:26 +0530

    pullcommit

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
-const { getGuestCount, getRegCount, getFlights, getPassCount, getPassAboveAgeDetails,getPassBelowAgeDetails, getPassDesCount,getTotalRevenueOfAircraft,updateDelayDetails,createFlight } = require("../controller/adminController");
+const { checkToken} = require("../auth/token_validation");
+const { isAdmin } = require("../middlewares/isAdmin");
+const { loginAdmin,getGuestCount, getRegCount, getFlights, getPassCount, getPassAboveAgeDetails,getPassBelowAgeDetails, getPassDesCount,getTotalRevenueOfAircraft,updateDelayDetails,createFlight } = require("../controller/adminController");
 
 
-router.post('/guest',getGuestCount);
-router.post('/register',getRegCount);
-router.post('/passflights',getFlights);
-router.post('/passcount', getPassCount);
-router.post('/delay', updateDelayDetails);
+router.post('/guest',checkToken,isAdmin,getGuestCount);
+router.post('/register',checkToken,isAdmin,getRegCount);
+router.post('/passflights',checkToken,isAdmin,getFlights);
+router.post('/passcount',checkToken,isAdmin, getPassCount);
+router.post('/delay',checkToken,isAdmin, updateDelayDetails);
 
-router.get('/passagedetails/above/:flight_id', getPassAboveAgeDetails);
-router.get('/passagedetails/below/:flight_id',getPassBelowAgeDetails);
-router.post('/passdescount', getPassDesCount);
-router.get('/totalrevenue/:aircraft_id', getTotalRevenueOfAircraft);
+router.get('/passagedetails/above/:flight_id',checkToken,isAdmin, getPassAboveAgeDetails);
+router.get('/passagedetails/below/:flight_id',checkToken,isAdmin,getPassBelowAgeDetails);
+router.post('/passdescount',checkToken,isAdmin, getPassDesCount);
+router.get('/totalrevenue/:aircraft_id',checkToken,isAdmin, getTotalRevenueOfAircraft);
 
-router.post('/createflight',createFlight);
+router.post('/createflight',checkToken,isAdmin,createFlight);
 
+router.post('/signin',loginAdmin);
 
 module.exports=router;
