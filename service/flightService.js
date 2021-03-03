ceb4d9e75182faf4c6e1987fbb638a71e1531ea1
unknown <kasun7153@gmail.com>
2021-03-03 22:40:25 +0530

    get Flight schedules witout expired ones

--- service/flightService.js
+++ service/flightService.js
@@ -6,7 +6,6 @@ module.exports = {
       `SELECT * FROM (SELECT * FROM flight_schedule natural join route) as a join (SELECT airport_id,country as origin_country,state  as origin_state ,city as origin_city  FROM airport) as b ON a.origin=b.airport_id join (SELECT airport_id,country as destination_country,state  as destination_state ,city as destination_city  FROM airport) as c ON a.destination=c.airport_id where date > CURDATE() order by date`,
       [],
       (err, result) => {
-        console.log(result)
         if (err) {
           return callBack(err);
         } else {
