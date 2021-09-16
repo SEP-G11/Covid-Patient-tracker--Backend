86045cf6cfe346a85a6b896244a52a2b589747f4
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-09-16 23:05:22 +0530

    facilitybed model

--- service/init-models.js
+++ service/init-models.js
@@ -3,6 +3,7 @@ var _Allocation = require("./Allocation");
 var _Bed = require("./Bed");
 var _DistrictStatus = require("./DistrictStatus");
 var _Facility = require("./Facility");
+var _FacilityBed = require("./FacilityBed");
 var _FacilityStaff = require("./FacilityStaff");
 var _MedicalReport = require("./MedicalReport");
 var _Patient = require("./Patient");
@@ -16,6 +17,7 @@ function initModels(sequelize) {
   var Bed = _Bed(sequelize, DataTypes);
   var DistrictStatus = _DistrictStatus(sequelize, DataTypes);
   var Facility = _Facility(sequelize, DataTypes);
+  var FacilityBed = _FacilityBed(sequelize, DataTypes);
   var FacilityStaff = _FacilityStaff(sequelize, DataTypes);
   var MedicalReport = _MedicalReport(sequelize, DataTypes);
   var Patient = _Patient(sequelize, DataTypes);
@@ -26,12 +28,16 @@ function initModels(sequelize) {
 
   Allocation.belongsTo(Bed, { as: "bed_no_bed", foreignKey: "bed_no"});
   Bed.hasMany(Allocation, { as: "allocations", foreignKey: "bed_no"});
+  Transfer.belongsTo(Bed, { as: "origin_bed", foreignKey: "origin_bed_id"});
+  Bed.hasMany(Transfer, { as: "transfers", foreignKey: "origin_bed_id"});
+  Transfer.belongsTo(Bed, { as: "destination_bed", foreignKey: "destination_bed_id"});
+  Bed.hasMany(Transfer, { as: "destination_bed_transfers", foreignKey: "destination_bed_id"});
   FacilityStaff.belongsTo(Facility, { as: "facility", foreignKey: "facility_id"});
   Facility.hasMany(FacilityStaff, { as: "facility_staffs", foreignKey: "facility_id"});
-  Transfer.belongsTo(Facility, { as: "origin_facility", foreignKey: "origin"});
-  Facility.hasMany(Transfer, { as: "transfers", foreignKey: "origin"});
-  Transfer.belongsTo(Facility, { as: "destination_facility", foreignKey: "destination"});
-  Facility.hasMany(Transfer, { as: "destination_transfers", foreignKey: "destination"});
+  MedicalReport.belongsTo(Facility, { as: "admitted_facility_facility", foreignKey: "admitted_facility"});
+  Facility.hasMany(MedicalReport, { as: "medical_reports", foreignKey: "admitted_facility"});
+  MedicalReport.belongsTo(Facility, { as: "discharged_facility_facility", foreignKey: "discharged_facility"});
+  Facility.hasMany(MedicalReport, { as: "discharged_facility_medical_reports", foreignKey: "discharged_facility"});
   Ward.belongsTo(Facility, { as: "facility", foreignKey: "facility_id"});
   Facility.hasMany(Ward, { as: "wards", foreignKey: "facility_id"});
   Test.belongsTo(MedicalReport, { as: "report", foreignKey: "report_id"});
@@ -48,11 +54,13 @@ function initModels(sequelize) {
   Ward.hasMany(Bed, { as: "beds", foreignKey: "ward"});
   DistrictStatus.removeAttribute('id');
   FacilityStaff.removeAttribute('id');
+  FacilityBed.removeAttribute("id");
   return {
     Allocation,
     Bed,
     DistrictStatus,
     Facility,
+    FacilityBed,
     FacilityStaff,
     MedicalReport,
     Patient,
