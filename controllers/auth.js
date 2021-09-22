77744807f8b22865217630e3dde1096007aac701
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-09-22 19:14:08 +0530

    forgot password, reset password routes+logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,69 +5,178 @@ const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 const config = require("config");
 const { successMessage, errorMessage } = require("../utils/message-template");
+const { sendResetPasswordEmail } = require("../utils/mailer");
+
 
 var User = models.User;
 var FacilityStaff = models.FacilityStaff;
+var PasswordReset = models.PasswordReset;
 
 function validateLogin(email,password) {
-  const schema = Joi.object({
-    email: Joi.string().email().trim().lowercase().required().label('Email'),
-    password: Joi.required().label('Password')
-  });
-  return schema.validate({ email: email, password: password })
+    const schema = Joi.object({
+        email: Joi.string().email().trim().lowercase().required().label('Email'),
+        password: Joi.required().label('Password')
+    });
+    return schema.validate({ email: email, password: password })
+}
+
+function validateForgotPassword(email){
+    const schema = Joi.object({
+        email: Joi.string().email().trim().lowercase().required().label('Email'),
+    });
+    return schema.validate({ email: email})
+}
+
+function validateResetPassword(password){
+    const schema = Joi.object({
+        password: Joi.string().trim().min(5).required().label('Password')
+    });
+    return schema.validate({ password: password })
 }
 
 
 const login = async (req, res, next) => {
-  const {email, password} = req.body;
-  const { error, value } = validateLogin(email,password);
-  if (error) {
-    return errorMessage(res, error.details[0].message, 422);
-  }
-  let loadedUser;
-  try {
-    const result = await User.findAll({
-      where: {
-        email: value.email,
-        is_deleted: 0
-      },
-      include: {
-        model: FacilityStaff,
-        as: 'facility_staffs'
-      }
-    });
-    if (!result.length>0) {
-      return errorMessage(res, 'Incorrect email or password', 401);
+    const {email, password} = req.body;
+    const { error, value } = validateLogin(email,password);
+    if (error) {
+        return errorMessage(res, error.details[0].message, 422);
     }
-    loadedUser = result[0].dataValues;
+    let loadedUser;
+    try {
+        const result = await User.findAll({
+            where: {
+                email: value.email,
+                is_deleted: 0
+            },
+            include: {
+                model: FacilityStaff,
+                as: 'facility_staffs'
+            }
+        });
+        if (!result.length>0) {
+            return errorMessage(res, 'Incorrect email or password', 401);
+        }
+        loadedUser = result[0].dataValues;
 
-    const isEqual = await bcrypt.compare(password,loadedUser.password);
-    if (!isEqual){
-      return errorMessage(res, 'Incorrect email or password', 401);
+        const isEqual = await bcrypt.compare(password,loadedUser.password);
+        if (!isEqual){
+            return errorMessage(res, 'Incorrect email or password', 401);
+        }
+        let tokenData = {
+            email: loadedUser.email,
+            userID: loadedUser.user_id.toString(),
+            accType: loadedUser.user_type
+        };
+        if (loadedUser.facility_staffs[0].dataValues.facility_id){
+            tokenData["facilityId"]=loadedUser.facility_staffs[0].dataValues.facility_id;
+        }
+
+        const token = jwt.sign(tokenData,
+            'somesupersecret',
+            {expiresIn: '30d'}                 //put in ENV
+        );
+        return successMessage(res, {
+            id: loadedUser.user_id.toString(),
+            email: loadedUser.email,
+            accType: loadedUser.user_type,
+            token: token
+        }, 'Logged in successfully')
     }
-    let tokenData = {
-      email: loadedUser.email,
-      userID: loadedUser.user_id.toString(),
-      accType: loadedUser.user_type,
-      expiresIn: 3600
-    };
-    if (loadedUser.facility_staffs[0].dataValues.facility_id){
-      tokenData["facilityId"]=loadedUser.facility_staffs[0].dataValues.facility_id;
+    catch (err) {
+        return errorMessage(res, 'Internal Server Error', 500);
     }
-    //console.log("hjdkskd")
-    const token = jwt.sign(tokenData,
-        'somesupersecret'                 //put in ENV
-    );
-    return successMessage(res, {
-      id: loadedUser.user_id.toString(),
-      email: loadedUser.email,
-      accType: loadedUser.user_type,
-      token: token
-    }, 'Logged in successfully')
-  }
-  catch (err) {
-    return errorMessage(res, 'Internal Server Error', 500);
-  }
+};
+
+const forgotPassword = async (req,res,next) => {
+    const {email} = req.body;
+    const { error, value } = validateForgotPassword(email);
+    if (error) {
+        return errorMessage(res, error.details[0].message, 422);
+    }
+    let loadedUser;
+    try {
+        const user = await User.findAll({
+            where: {
+                email: value.email,
+                is_deleted: 0
+            }
+        });
+
+        if (!user.length>0) {
+            return errorMessage(res, 'Entered Email does not match an account', 404);
+        }
+        loadedUser = user[0].dataValues;
+
+        const tokenData = {
+            email: loadedUser.email,
+        };
+
+        const token = jwt.sign(tokenData,
+            'forgotpwsecret',                 //put in ENV
+            {expiresIn: '20m'}
+        );
+
+        const result = await PasswordReset.create({
+            email: value.email,
+            token: token
+        });
+
+        if (result){
+            sendResetPasswordEmail(value.email,token);
+            return successMessage(res,{},"Email sent, Please check you Email")
+        }
+
+    }
+    catch (err) {
+        return errorMessage(res, 'Internal Server Error', 500);
+    }
+
+};
+
+const resetPassword = async (req,res,next) => {
+    const {password,token} = req.body;
+    const { error, value } = validateResetPassword(password);
+    if (error) {
+        return errorMessage(res, error.details[0].message, 422);
+    }
+    let t;
+    try{
+        const {email,iat,exp} = jwt.verify(token,'forgotpwsecret');
+        const pwResetRequest = await PasswordReset.findAll({where:{
+                email: email,
+                token: token,
+                is_used: 0
+            }
+        });
+        if (!pwResetRequest.length>0) {
+            return errorMessage(res, 'Password reset failed', 400);
+        }
+
+        if (Date.now() >= exp * 1000){
+            return errorMessage(res, 'Link expired', 400);
+        }
+
+        const hashedPw = await bcrypt.hash(value.password, 12);
+
+        t =  await sequelize.transaction();
+
+        const resetPw = await User.update({password: hashedPw},{where: {email: email}},{ transaction: t });
+        const updateTokenUsed = await PasswordReset.update({is_used: 1},{where: { token: token } },{ transaction: t });
+
+        await t.commit();
+        if (resetPw && updateTokenUsed){
+            return successMessage(res,{},'Password reset successful', 200);
+        }
+        else {
+            return errorMessage(res, 'Password reset failed', 400);
+        }
+
+    }
+    catch (err) {
+        await t.rollback();
+        return errorMessage(res, 'Internal Server Error', 500);
+    }
+
 };
 
 
@@ -121,5 +230,5 @@ const login = async (req, res, next) => {
 // };
 
 module.exports = {
-  login
+    login,forgotPassword,resetPassword
 };
