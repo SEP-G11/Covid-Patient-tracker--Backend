a7e22a0fb61134310cf96d35107e3cecad6a267f
Chathulanka Gamage <61736812+cmgchess@users.noreply.github.com>
2021-09-24 16:51:26 +0530

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -1,11 +1,11 @@
-const {enterResult} = require("../controllers/test");
+const {enterResult,getTestDetailsById} = require("../controllers/test");
 const express = require("express");
 const router = express.Router();
 const {protect,authorize} = require('../middlewares/authorization');
 
 
 router.post('/enter/',protect,authorize(['DOC','HA']),enterResult);
-
+router.get('/testDetails/:id', protect,authorize(['DOC']),getTestDetailsById);
 
 
 module.exports = router;
