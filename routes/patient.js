e95a26b9534b3ce87d95370a954b5033787907fd
Chathulanka Gamage <61736812+cmgchess@users.noreply.github.com>
2021-09-24 16:52:50 +0530

    Update patient.js

--- routes/patient.js
+++ routes/patient.js
@@ -1,4 +1,4 @@
-const {admitPatient,dischargePatient,transferPatient} = require("../controllers/patient");
+const {admitPatient,dischargePatient,transferPatient,getPatients,getPatientById,updatePatient} = require("../controllers/patient");
 const express = require("express");
 const router = express.Router();
 const {protect,authorize} = require('../middlewares/authorization');
@@ -10,6 +10,9 @@ const {protect,authorize} = require('../middlewares/authorization');
 router.post('/admit/',protect,authorize(['HA','DOC']),admitPatient);
 router.post('/discharge/',protect,authorize(['HA','DOC']), dischargePatient);
 router.post('/transfer/',protect,authorize(['HA','DOC']),transferPatient);
+router.get('/getPatients',protect,authorize(['HA','DOC']),getPatients);
+router.get('/patientDetails/:id',protect,authorize(['HA','DOC']),getPatientById);
+router.put('/updatePatient/:id', protect,authorize(['HA','DOC']), updatePatient);
 
 
 
