f675149252e5ea8a2a956bca254b449b7418ba4d
Chathulanka Gamage <61736812+cmgchess@users.noreply.github.com>
2021-09-24 16:52:01 +0530

    Update report.js

--- routes/report.js
+++ routes/report.js
@@ -1,11 +1,12 @@
-const {createReport} = require("../controllers/report");
+const {createReport,getPatientReportById,updateReport} = require("../controllers/report");
 const express = require("express");
 const router = express.Router();
 const {protect,authorize} = require('../middlewares/authorization');
 
 
 router.post('/createReport/',protect,authorize(['DOC']),createReport);
-
+router.get('/patientReportDetails/:id', protect,authorize(['DOC']), getPatientReportById);
+router.put('/updateReport/:id', protect,authorize(['DOC']), updateReport);
 
 
 module.exports = router;
