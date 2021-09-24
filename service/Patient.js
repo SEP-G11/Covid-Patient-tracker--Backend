1caa2fd12e47a3c87f41b6ed58694de73eea78ee
Chathulanka Gamage <61736812+cmgchess@users.noreply.github.com>
2021-09-24 16:29:33 +0530

    Update Patient.js

--- service/Patient.js
+++ service/Patient.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Patient', {
     patient_id: {
-      type: DataTypes.STRING(12),
+      type: DataTypes.STRING(200),
       allowNull: false,
       primaryKey: true
     },
