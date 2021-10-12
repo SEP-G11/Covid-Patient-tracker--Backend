31ec9c8db60de5fca0af4cb9b3f9b8f5eece6c16
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-10-12 16:47:47 +0530

    model import cleanup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,11 @@
-// const sequelize = require('../database/db');
-// var models = require("../service/init-models").initModels(sequelize);
 const jwt = require("jsonwebtoken");
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 const { successMessage, errorMessage } = require("../utils/message-template");
 const { sendResetPasswordEmail } = require("../utils/mailer");
 
-
-// var User = models.User;
-// var FacilityStaff = models.FacilityStaff;
-// var PasswordReset = models.PasswordReset;
 const {User,FacilityStaff,PasswordReset,sequelize} = require('../service/models')
+
 function validateLogin(email,password) {
     const schema = Joi.object({
         email: Joi.string().email().trim().lowercase().required().label('Email'),
