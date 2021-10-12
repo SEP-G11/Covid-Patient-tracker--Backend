0fa16adfa1042d15e303298fe5987d298c6809cb
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-10-12 21:53:31 +0530

    db

--- database/db.js
+++ database/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('covid_test','root','',{
+const dbName = process.env.NODE_ENV !== 'test'? 'corona_test_test': 'coronatracker_testing_db'
+
+const sequelize = new Sequelize(dbName,'root','',{
     dialect: 'mysql',
     host: 'localhost',
     dialectOptions:{
