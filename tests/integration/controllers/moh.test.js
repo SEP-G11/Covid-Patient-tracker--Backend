fbf17c269cb54a3aea0a318ec6aa2b398c47d529
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-10-13 19:39:19 +0530

    moh I&T

--- tests/integration/controllers/moh.test.js
+++ tests/integration/controllers/moh.test.js
@@ -1,29 +1,30 @@
-const { overallCountryStats,overallDistrictStats,overallDistrictsStats} = require('../../../controllers/moh');
- const sequelize = require('../../../database/db');
-// var models = require("../../../service/init-models").initModels(sequelize);
-//var DistrictStatus = models.DistrictStatus;
-const {DistrictStatus} = require('../../../service/models');
+const { overallCountryStats,overallDistrictStats,overallDistrictsStats,
+    historicalTests,historicalDeaths,historicalRecovered,historicalCases,
+    facilityHistorical,getFacilitiesBeds,getFacilitiesActive,getFacilitiesDeaths,
+    getFacilitiesRecovered,getFacilities,register} = require('../../../controllers/moh');
+const {DistrictStatus,MedicalReport,Test,sequelize} = require('../../../service/models');
 let server;
 
 describe('MOH Controller', () => {
-    describe('overallCountryStats', () => {
-        const req = {
-            query: {},
-            params:{}
-        };
-        const res = {
-            status: jest.fn(() => res),
-            send: jest.fn(),
-            end: jest.fn()
-        };
-        const next = jest.fn();
+    const req = {
+        query: {},
+        params:{}
+    };
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(),
+        end: jest.fn()
+    };
+    const next = jest.fn();
 
-        beforeEach(async () => {
-            server = require('../../../index');
-        });
-        afterEach(async () => {
-            await server.close();
-        });
+    beforeEach(async () => {
+        server = require('../../../index');
+    });
+    afterEach(async () => {
+        await server.close();
+        jest.restoreAllMocks();
+    });
+    describe('overallCountryStats', () => {
 
         it("should return 200 and send country stats if found", async () => {
             // const expectedUser = 1;
@@ -40,35 +41,24 @@ describe('MOH Controller', () => {
         });
 
         it("should return 404 and not found message if no country stats", async () => {
-            DistrictStatus.findAll = jest.fn().mockImplementation(() => {
-                return Promise.resolve([])
-            });
-            const spy = jest.spyOn(DistrictStatus, "findAll")
+            jest.spyOn(DistrictStatus, "findAll").mockImplementation(() => {return Promise.resolve([])});
 
-           await overallCountryStats(req, res,next);//TODO
+            const expectedOutput = { object : null, message: 'Country Data Not Found' };
+            await overallCountryStats(req, res,next);
             expect(res.status).toBeCalledWith(404);
-           // expect(res.send).toHaveBeenCalledWith({ object : null, message: 'Country Data Not Found' });
+            expect(res.send).toHaveBeenCalledWith(expectedOutput);
+        });
+        it("should return 500 if Internal server error", async () => {
+            jest.spyOn(DistrictStatus, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
+
+            const expectedOutput = {object : null, message: 'Internal Server Error'};
+            await overallCountryStats(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
 
     });
     describe('overallDistrictStats', () => {
-        const req = {
-            query: {},
-            params:{}
-        };
-        const res = {
-            status: jest.fn(() => res),
-            send: jest.fn(),
-            end: jest.fn()
-        };
-        const next = jest.fn();
-
-        beforeEach(async () => {
-            server = require('../../../index');
-        });
-        afterEach(async () => {
-            await server.close();
-        });
 
         it("should return 200 and send district stats if found", async () => {
             req.params.district = 'Colombo';
@@ -94,38 +84,24 @@ describe('MOH Controller', () => {
         it("should return 404 and not found message if district stats not found", async () => {
             req.params.district = 'sdfsdhfds';
 
+            const expectedOutput = { object : null, message: 'District Data Not Found' };
             await overallDistrictStats(req, res,next);
             expect(res.status).toBeCalledWith(404);
-            expect(res.send).toHaveBeenCalledWith({ object : null, message: 'District Data Not Found' });
+            expect(res.send).toHaveBeenCalledWith(expectedOutput);
         });
         it("should return 500 if Internal server error", async () => {
-            req.params.district = new Error();
+            req.params.district = 'Colombo';
 
+            const expectedOutput = { object : null, message: 'Internal Server Error' };
+            jest.spyOn(DistrictStatus, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
             await overallDistrictStats(req, res,next);
             expect(res.status).toBeCalledWith(500);
-            expect(res.send).toHaveBeenCalledWith({ object : null, message: 'Internal Server Error' });
+            expect(res.send).toHaveBeenCalledWith(expectedOutput);
         });
 
     });
 
     describe('overallDistrictsStats', () => {
-        const req = {
-            query: {},
-            params:{}
-        };
-        const res = {
-            status: jest.fn(() => res),
-            send: jest.fn(),
-            end: jest.fn()
-        };
-        const next = jest.fn();
-
-        beforeEach(async () => {
-            server = require('../../../index');
-        });
-        afterEach(async () => {
-            await server.close();
-        });
 
         it("should return 200 and send districts stats if found", async () => {
             req.params.district = 'Colombo';
@@ -212,74 +188,176 @@ describe('MOH Controller', () => {
             expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 404 and not found message if districts stats not found", async () => {
-            const mockModels = {
-                DistrictStatus: {
-                    findAll: jest.fn(() => Promise.resolve([]))
-                }
-            };
-            jest.mock('../../../service/init-models', () => ({
-                initModels: jest.fn(() => mockModels),
-            }));
-            const sequelize = require('../../../database/db');
-            var models = require("../../../service/init-models").initModels(sequelize);
-            console.log(await models.DistrictStatus.findAll())
-//TODO
-            //console.log(mockModels)
-            await overallDistrictsStats(req,res,next)
+            jest.spyOn(DistrictStatus, "findAll").mockImplementation(() => {return Promise.resolve([])});
+
+            const expectedOutput = {object: null,message:"Districts Data Not Found"};
+            await overallDistrictsStats(req,res,next);
             expect(res.status).toBeCalledWith(404);
-            //expect(res.send).toHaveBeenCalledWith('hello')
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(DistrictStatus, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await overallDistrictsStats(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
 
     });
 
     describe('historicalCases', () => {
-        it("should return 200 and send historical cases if found", async () => {
-
+        it("should return 200 and send historical cases for given last days number", async () => {
+            req.query.lastdays = '5';
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0,
+                "10/12/21": 0,
+                "10/11/21": 0,
+                "10/10/21": 0,
+                "10/9/21": 0
+            };
+            const expectedOutput = {results: expectedResult,message:"Historical Cases over last 5 days Found"};
+            await historicalCases(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
-        it("should return 404 and not found message if historical cases not found", async () => {
+        it("should return 200 and send historical cases of last 1 day if last days number not given", async () => {
+            req.query.lastdays = undefined;
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0
+            };
 
+            const expectedOutput = {results: expectedResult,message:"Historical Cases over last 1 days Found"};
+            await historicalCases(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            req.query.lastdays = '5';
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await historicalCases(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('historicalRecovered', () => {
-        it("should return 200 and send historical recovered if found", async () => {
-
+        it("should return 200 and send historical recovered for given last days number", async () => {
+            req.query.lastdays = '5';
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0,
+                "10/12/21": 0,
+                "10/11/21": 0,
+                "10/10/21": 0,
+                "10/9/21": 0
+            };
+            const expectedOutput = {results: expectedResult,message:"Historical Recovered over last 5 days Found"};
+            await historicalRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
-        it("should return 404 and not found message if historical recovered not found", async () => {
+        it("should return 200 and send historical recovered of last 1 day if last days number not given", async () => {
+            req.query.lastdays = undefined;
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0
+            };
 
+            const expectedOutput = {results: expectedResult,message:"Historical Recovered over last 1 days Found"};
+            await historicalRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            req.query.lastdays = '5';
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await historicalRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('historicalDeaths', () => {
-        it("should return 200 and send historical deaths if found", async () => {
-
+        it("should return 200 and send historical deaths for given last days number", async () => {
+            req.query.lastdays = '5';
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0,
+                "10/12/21": 0,
+                "10/11/21": 0,
+                "10/10/21": 0,
+                "10/9/21": 0
+            };
+            const expectedOutput = {results: expectedResult,message:"Historical Deaths over last 5 days Found"};
+            await historicalDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
-        it("should return 404 and not found message if historical deaths not found", async () => {
+        it("should return 200 and send historical deaths of last 1 day if last days number not given", async () => {
+            req.query.lastdays = undefined;
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": 0
+            };
 
+            const expectedOutput = {results: expectedResult,message:"Historical Deaths over last 1 days Found"};
+            await historicalDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            req.query.lastdays = '5';
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await historicalDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('historicalTests', () => {
-        it("should return 200 and send historical tests if found", async () => {
-
+        it("should return 200 and send historical tests for given last days number", async () => {
+            req.query.lastdays = '5';
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": {"pcr": 0, "rat": 0},
+                "10/12/21": {"pcr": 0, "rat": 0},
+                "10/11/21": {"pcr": 0, "rat": 0},
+                "10/10/21": {"pcr": 0, "rat": 0},
+                "10/9/21": {"pcr": 0, "rat": 0}
+            };
+            const expectedOutput = {results: expectedResult,message:"Historical Tests over last 5 days Found"};
+            await historicalTests(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
-        it("should return 404 and not found message if historical tests not found", async () => {
-
+        it("should return 200 and send historical tests of last 1 day if last days number not given", async () => {
+            req.query.lastdays = undefined;
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {
+                "10/13/21": {"pcr": 0, "rat": 0}
+            };
+            const expectedOutput = {results: expectedResult,message:"Historical Tests over last 1 days Found"};
+            await historicalTests(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            req.query.lastdays = '5';
+            jest.spyOn(Test, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await historicalTests(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
