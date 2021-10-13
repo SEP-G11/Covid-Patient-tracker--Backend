34e2ba08fd9a47c4057d8670bb83a234f7319191
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-10-14 01:42:37 +0530

    separated validation schemas

--- controllers/moh.js
+++ controllers/moh.js
@@ -4,22 +4,10 @@ const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 const { successMessage, errorMessage } = require("../utils/message-template");
 const { districtStatsMutate,countryStatsMutate,dateMapToValuesMutate,dateMapToTestsMutate,facilityBedsMutate} = require("../utils/array-mutation");
+const {validateRegister} = require('../utils/validationSchemas/mohValidationSchemas');
 var fs = require('fs');
 
 const {User,DistrictStatus,MedicalReport,Test,FacilityStaff,Facility,FacilityBed,sequelize} = require('../service/models');
-
-function validateRegister(id, name, email, contact, password,accountType,facilityId) {
-    const schema = Joi.object({
-        id: Joi.string().pattern(/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/).required().label('ID').messages({'string.pattern.base': '"ID" must be a valid NIC'}),
-        email: Joi.string().email().trim().lowercase().max(100).required().label('Email'),
-        name: Joi.string().trim().max(255).required().label('Name'),
-        contact: Joi.string().max(12).required().label('Contact'),
-        password: Joi.string().trim().min(5).required().label('Password'),
-        accountType: Joi.string().valid('MOH','DOC','HA').required().label('Account Type'),
-        facilityId: Joi.number().when('accountType',{not: 'MOH',then: Joi.number().required()}).label("Facility")
-    });
-    return schema.validate({ id: id, email: email, name: name, contact: contact, password: password, accountType: accountType, facilityId: facilityId })
-}
 
 const register = async (req, res, next) => {
     const {id,email,name,contact,password,accountType,facilityId} = req.body;
