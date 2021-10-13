3485a14fb3dffaedac4ba27ed5742ed34707570b
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-10-13 23:20:59 +0530

    MOH I&T

--- tests/integration/controllers/moh.test.js
+++ tests/integration/controllers/moh.test.js
@@ -2,7 +2,7 @@ const { overallCountryStats,overallDistrictStats,overallDistrictsStats,
     historicalTests,historicalDeaths,historicalRecovered,historicalCases,
     facilityHistorical,getFacilitiesBeds,getFacilitiesActive,getFacilitiesDeaths,
     getFacilitiesRecovered,getFacilities,register} = require('../../../controllers/moh');
-const {DistrictStatus,MedicalReport,Test,sequelize} = require('../../../service/models');
+const {DistrictStatus,MedicalReport,Test,Facility,FacilityBed,sequelize} = require('../../../service/models');
 let server;
 
 describe('MOH Controller', () => {
@@ -363,73 +363,318 @@ describe('MOH Controller', () => {
 
     describe('getFacilities', () => {
         it("should return 200 and send facilities if found", async () => {
-
+            await getFacilities(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities found"
+                })
+            );
         });
         it("should return 404 and not found message if facilities not found", async () => {
+            jest.spyOn(Facility, "findAll").mockImplementation(() => {return Promise.resolve([])});
 
+            const expectedOutput = {object: null,message:"Facilities Not Found"};
+            await getFacilities(req,res,next);
+            expect(res.status).toBeCalledWith(404);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(Facility, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await getFacilities(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('getFacilitiesRecovered', () => {
         it("should return 200 and send facilities recovered count if found", async () => {
+            req.query.facility = null;
 
+            await getFacilitiesRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities recoveries found",
+                    results: [expect.any(MedicalReport),expect.any(MedicalReport),expect.any(MedicalReport),expect.any(MedicalReport),expect.any(MedicalReport)]
+                })
+            )
+        });
+        it("should return 200 and send facility recovered count if found", async () => {
+            req.query.facility = '1';
+            await getFacilitiesRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities recoveries found",
+                    results: [expect.any(MedicalReport)]
+                })
+            );
         });
         it("should return 404 and not found message if facilities recovered count not found", async () => {
+            req.query.facility = '150';  //not in DB
 
+            const expectedOutput = {object: null,message:"Facilities recoveries Not Found"};
+            await getFacilitiesRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(404);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await getFacilitiesRecovered(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('getFacilitiesDeaths', () => {
         it("should return 200 and send facilities deaths count if found", async () => {
+            req.query.facility = null;
 
+            await getFacilitiesDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities deaths found",
+                    results: [expect.any(MedicalReport),expect.any(MedicalReport)]
+                })
+            )
+        });
+        it("should return 200 and send facility deaths count if found", async () => {
+            req.query.facility = '2';
+            await getFacilitiesDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities deaths found",
+                    results: [expect.any(MedicalReport)]
+                })
+            );
         });
         it("should return 404 and not found message if facilities deaths count not found", async () => {
+            req.query.facility = '150';  //not in DB
 
+            const expectedOutput = {object: null,message:"Facilities deaths Not Found"};
+            await getFacilitiesDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(404);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await getFacilitiesDeaths(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('getFacilitiesActive', () => {
         it("should return 200 and send facilities active count if found", async () => {
+            req.query.facility = null;
 
+            await getFacilitiesActive(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities active found",
+                    results: [expect.any(MedicalReport),expect.any(MedicalReport),expect.any(MedicalReport)]
+                })
+            )
+        });
+        it("should return 200 and send facility active count if found", async () => {
+            req.query.facility = '1';
+            await getFacilitiesActive(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    "message": "Facilities active found",
+                    results: [expect.any(MedicalReport)]
+                })
+            );
         });
         it("should return 404 and not found message if facilities active count not found", async () => {
+            req.query.facility = '150';  //not in DB
 
+            const expectedOutput = {object: null,message:"Facilities active Not Found"};
+            await getFacilitiesActive(req,res,next);
+            expect(res.status).toBeCalledWith(404);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await getFacilitiesActive(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('getFacilitiesBeds', () => {
         it("should return 200 and send facilities beds count if found", async () => {
-
+            const expectedResult = {"4": {"totalCovidBeds": 10, "occupiedCovidBeds": 2, "totalNormalBeds": 10, "occupiedNormalBeds": 0},
+                "8": {"totalCovidBeds": 10, "occupiedCovidBeds": 0, "totalNormalBeds": 10, "occupiedNormalBeds": 0},
+                "12": {"totalCovidBeds": 5, "occupiedCovidBeds": 0, "totalNormalBeds": 5, "occupiedNormalBeds": 0}}
+            const expectedOutput = {results: expectedResult,message:"Facilities Beds data found"};
+            await getFacilitiesBeds(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 404 and not found message if facilities beds count not found", async () => {
+            jest.spyOn(FacilityBed, "findAll").mockImplementation(() => {return Promise.resolve([])});
 
+            const expectedOutput = {object: null,message:"Facilities Beds data Not Found"};
+            await getFacilitiesBeds(req,res,next);
+            expect(res.status).toBeCalledWith(404);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(FacilityBed, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await getFacilitiesBeds(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
     describe('facilityHistorical', () => {
-        it("should return 200 and send facility historical data if found", async () => {
+        it("should return 200 and send facility historical data - default", async () => {
+            req.query = {
+                lastdays: undefined,
+                type: undefined,
+                facility: undefined
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0};
 
+            const expectedOutput = {results: expectedResult,message:"Historical cases of facility 1 over last 1 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
-        it("should return 404 and not found message if facility historical data not found", async () => {
+        it("should return 200 and send facility historical data - specify lastDays only", async () => {
+            req.query={
+                lastdays: 5,
+                type: undefined,
+                facility: undefined
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0, "10/12/21": 0, "10/11/21": 0, "10/10/21": 0, "10/9/21": 0};
 
+            const expectedOutput = {results: expectedResult,message:"Historical cases of facility 1 over last 5 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify caseType only", async () => {
+            req.query={
+                lastdays: undefined,
+                type: 'deaths',
+                facility: undefined
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical deaths of facility 1 over last 1 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify facilityId only", async () => {
+            req.query={
+                lastdays: undefined,
+                type: undefined,
+                facility: 2
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical cases of facility 2 over last 1 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify lastDays,caseType", async () => {
+            req.query={
+                lastdays: 5,
+                type :'deaths',
+                facility: undefined
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0, "10/12/21": 0, "10/11/21": 0, "10/10/21": 0, "10/9/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical deaths of facility 1 over last 5 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify lastDays,facilityId", async () => {
+            req.query={
+                lastdays: 5,
+                type :undefined,
+                facility :2
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0, "10/12/21": 0, "10/11/21": 0, "10/10/21": 0, "10/9/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical cases of facility 2 over last 5 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify caseType,facilityId", async () => {
+            req.query={
+                lastdays: undefined,
+                type: 'deaths',
+                facility :2
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical deaths of facility 2 over last 1 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 200 and send facility historical data - specify lastDays,caseType,facilityId", async () => {
+            req.query={
+                type: 'deaths',
+                facility :2,
+                lastdays: 5
+            };
+            Date.now = jest.fn(() => new Date("2021-10-13T12:33:37.000Z"));
+            const expectedResult = {"10/13/21": 0, "10/12/21": 0, "10/11/21": 0, "10/10/21": 0, "10/9/21": 0};
+
+            const expectedOutput = {results: expectedResult,message:"Historical deaths of facility 2 over last 5 days Found"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
+        });
+        it("should return 422 and error message if invalid case type", async () => {
+            req.query={
+                type: 'deathssssss',
+                facility :2,
+                lastdays: 5
+            };
+
+            const expectedOutput = {object: null,message:'Invalid case type'};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(422);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
         it("should return 500 if Internal server error", async () => {
+            jest.spyOn(MedicalReport, "findAll").mockImplementation(() => {return Promise.reject(new Error('Mock DB Error'))});
 
+            const expectedOutput = {object: null,message:"Internal Server Error"};
+            await facilityHistorical(req,res,next);
+            expect(res.status).toBeCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(expectedOutput)
         });
     });
 
